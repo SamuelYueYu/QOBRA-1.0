bfa16379bd103a85701c624f8c2806f6ac681d65
Samuel Yue Yu <109395848+SamuelYueYu@users.noreply.github.com>
2025-03-10 10:47:28 -0400

    Update run-Ca.sh

--- run-Ca.sh
+++ run-Ca.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 2:30:00
+#SBATCH -t 15:00:00
 #SBATCH -C gpu
 #SBATCH -c 128
 #SBATCH --gpus-per-task=4
 #SBATCH -q regular
-#SBATCH -J Ca-0.5-1-d
+#SBATCH -J Ca
 #SBATCH --output=%x.out
 #SBATCH -A m4444
 
 module load python
-srun python train.py 6 Ca 0
-srun python generate.py 6 Ca 1
+srun python train.py Ca 9 2 0
+srun python gen.py Ca 9 2 0
